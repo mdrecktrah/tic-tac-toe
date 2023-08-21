19a13875cd174d908ca1b81b4d1354e006a584c4
mdrecktrah <maggorr@Maggo-PC>
2023-08-21 11:39:49 +0200

    Added patch notes. Also release of version 0.2

---
+++ patch-notes.md
@@ -0,0 +1,22 @@
+# To-Dos
+- Write a short manual / explanation of rules
+- Coin toss as option for who will start the game
+- Let players choose their marks
+- Set default names if no input is given for player names
+- Prompt at the end of the game to start a new game with the same players and marks right away
+- Improve error message for taken spot
+- Create a visual interface
+
+# Version history
+
+## Version 0.2 - 2023/08/21
+Changes:
+- Display an error message when a spot is already taken or spot selected is out of bounds and trigger re-select by active player
+
+## Version 0.1 - 2023/08/18
+The very first release! With this you are able to play a very simple and straightforward game of Tic-Tac-Toe against a friend (or yourself, there is nothing wrong with being your own best friend =)<br>
+Functionality includes:
+- Give names to Player 1 ('O' mark) and Player 2 ('X' mark)
+- Alternate setting your marks with Player 1 starting on a 3x3 matrix by choosing fields 1-9
+- A maximum of 9 turns, after which a draw will be called
+- If any player manages to get 3 marks in a row, a win message will be displayed
--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,21 +1,18 @@
+# Version 0.2
+
 # Create the board
 def create_board():
     row1 = [1, 2, 3]
     row2 = [4, 5, 6]
     row3 = [7, 8, 9]
-    board = [
-        row1,
-        row2,
-        row3
-    ]
+    board = [row1, row2, row3]
     return board
 
 # Print the board
 def print_board(board):
     print("")
-    print(f"| {board[0][0]} | {board[0][1]} | {board[0][2]} |")
-    print(f"| {board[1][0]} | {board[1][1]} | {board[1][2]} |")
-    print(f"| {board[2][0]} | {board[2][1]} | {board[2][2]} |")
+    for row in board:
+        print(f"| {row[0]} | {row[1]} | {row[2]} |")
     print("")
     return board
 
@@ -30,6 +27,25 @@ def check_win(board):
     board[0][1] == board[1][1] == board[2][1] or\
     board[0][2] == board[1][2] == board[2][2]:
         return(True)
+    
+# Check if chosen spot is valid and/or still available:
+def check_spot(board, spot):
+    if 1 <= spot <= 3:
+        idx = 0
+        spot -= 1
+    elif 4 <= spot <= 6:
+        idx = 1
+        spot -= 4
+    elif 7 <= spot <= 9:
+        idx = 2
+        spot -= 7
+    else:
+        return False
+    
+    if board[idx][spot] == "O" or board[idx][spot] == "X":
+        return False
+    else:
+        return True
 
 # Tic-tac-toe game
 if __name__ == "__main__":
@@ -45,34 +61,38 @@ if __name__ == "__main__":
         print_board(board)
         if turn % 2 != 0:
             P1_set_mark = int(input(f"{P1_name}, set your mark (Choose 1-9). "))
-            for row in board:
-                for idx in row:
-                    if P1_set_mark == idx:
-                        if 1 <= P1_set_mark <= 3:
-                            row[idx-1] = "O"
-                        elif 4 <= P1_set_mark <= 6:
-                            row[idx-4] = "O"
-                        elif 7 <= P1_set_mark <= 9:
-                            row[idx-7] = "O"
-                        else:
-                            print("Please choose another position.")
+            if check_spot(board, P1_set_mark) == False:
+                print("Please choose another position.")
+                continue
+            else:
+                for row in board:
+                    for idx in row:
+                        if P1_set_mark == idx:
+                            if 1 <= P1_set_mark <= 3:
+                                row[idx-1] = "O"
+                            elif 4 <= P1_set_mark <= 6:
+                                row[idx-4] = "O"
+                            elif 7 <= P1_set_mark <= 9:
+                                row[idx-7] = "O"
             if check_win(board) == True:
                 print_board(board)
                 print(f"Congratulations {P1_name}, you won!")
                 break
         else:
             P2_set_mark = int(input(f"{P2_name}, set your mark (Choose 1-9). "))
-            for row in board:
-                for idx in row:
-                    if P2_set_mark == idx:
-                        if 1 <= P2_set_mark <= 3:
-                            row[idx-1] = "X"
-                        elif 4 <= P2_set_mark <= 6:
-                            row[idx-4] = "X"
-                        elif 7 <= P2_set_mark <= 9:
-                            row[idx-7] = "X"
-                        else:
-                            print("Please choose another position.")
+            if check_spot(board, P2_set_mark) == False:
+                print("Please choose another position.")
+                continue
+            else:
+                for row in board:
+                    for idx in row:
+                        if P2_set_mark == idx:
+                            if 1 <= P2_set_mark <= 3:
+                                row[idx-1] = "X"
+                            elif 4 <= P2_set_mark <= 6:
+                                row[idx-4] = "X"
+                            elif 7 <= P2_set_mark <= 9:
+                                row[idx-7] = "X"
             if check_win(board) == True:
                 print_board(board)
                 print(f"Congratulations {P2_name}, you won!")
